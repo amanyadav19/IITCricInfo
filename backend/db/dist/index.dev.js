a88a6a64e553a19ac2480010e7f9934409b8f77c
Ritik Mandal <ritik02mandal@gmail.com>
2022-02-07 16:00:13 +0530

    players bulleted list

--- backend/db/dist/index.dev.js
+++ backend/db/dist/index.dev.js
@@ -3,13 +3,7 @@
 var _require = require('pg'),
     Pool = _require.Pool;
 
-var pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "lab3",
-  password: "king.12345",
-  port: 5432
-});
+var pool = new Pool();
 module.exports = {
   query: function query(text, params) {
     return pool.query(text, params);
